0494ac995eac1e3e725789df43d5c96468fb4f5f
KrzysztofMamak <mamako@onet.pl>
2022-06-03 21:17:15 +0200

    Initial

---
+++ android/app/src/main/kotlin/com/mamak/metrics_viewer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mamak.metrics_viewer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:metrics_viewer/app.dart';
+
+void main() {
+  // runApp(const App());
+}
