6747c2d81cd8955eb38fb0c3a5ef0ec87a6c2038
KrzysztofMamak <mamako@onet.pl>
2022-06-23 23:05:31 +0200

    Specify supportedLocales and localizationsDelegates

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:metrics_viewer/app_router.dart';
 import 'package:metrics_viewer/injection/injection.dart';
 
@@ -11,6 +13,12 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     final router = getIt<AppRouter>();
     return MaterialApp.router(
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizations.supportedLocales,
       routeInformationParser: router.defaultRouteParser(),
       routerDelegate: router.delegate(),
     );
