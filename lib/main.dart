77d75afabba783a405111026227be5daba8d3779
KrzysztofMamak <mamako@onet.pl>
2022-06-23 23:05:30 +0200

    Implement main function, configuration and runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:metrics_viewer/app.dart';
+import 'package:metrics_viewer/app_config_manager.dart';
+import 'package:metrics_viewer/injection.dart';
 
-void main() {
-  // runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppConfigManager.configure();
+  configureInjection();
+  runApp(const App());
 }
